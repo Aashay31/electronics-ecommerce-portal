77d55e18943ecf9d6d8b7358b708401d83f3709a
Ash1912-dev <aashayvaidya4@gmail.com>
2026-05-13 17:26:43 +0530

    Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/database");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Backend server is running");
